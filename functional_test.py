f984ae837728c3abe3f586e8795634bc15e81eb3
Maycon Ferreira <mayconferr.18@gmail.com>
2020-12-15 09:07:16 -0300

    Added FT for validate lists in template

--- functional_test.py
+++ functional_test.py
@@ -3,10 +3,14 @@ import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
+        options = Options()
+        # options.add_argument('--headless')
+
         self.browser = webdriver.Firefox()
 
     def tearDown(self) -> None:
@@ -15,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouviu falar de uma nova aplicação online interessante para
         # listas de tarefas. Ela decide verificar sua home page
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8010')
 
         # Ela percebe que o titulo da pagína e o cabeçalho mencionam listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
@@ -42,16 +46,23 @@ class NewVisitorTest(unittest.TestCase):
         # 1: Buy peacock feathers" como um item em uma lista de tarefas
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f'New to-do item did not appear in table. Contents were: \n {table.text}'
+        # )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item
+        inputbox = self.browser.find_element_by_id('id_new_item')
         # Ela insere "Use peacock feathers to make a fly"
-
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # A pagína e atualizada novamente e agora mostra os dois itens em sua lista
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         # Edith se pergunta se o site lembrará de sua lista.
         # Então ela nota que o site gerou um URL unico para ela --há um pequeno texto explicativo para isso
 
