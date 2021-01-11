97e38cb40e2a07c6b963e1fe3650b6c64d5880f8
Maycon Ferreira <mayconferr.18@gmail.com>
2021-01-11 14:51:37 -0300

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -142,7 +142,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.findgit_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
