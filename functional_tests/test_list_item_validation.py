c290c6fa3ad49cb4419acd14bcd3f6ea8fc65c55
Maycon Ferreira <mayconferr.18@gmail.com>
2021-01-18 09:41:41 -0300

    Rename all item input ids and names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,7 @@ class ItemValidationTest(FunctionalTest):
         # Edith acessa a pagina inicial e acidentalmente tenta submeter
         # um item vazio na lista. Ela tecla Enter na caixa de entrada
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
         # A pagina inicial é atualizada e ha uma mensagem de erro informando
         # que itens da lista não podem estar em branco
         self.wait_for(lambda: self.assertEqual(
@@ -18,13 +18,13 @@ class ItemValidationTest(FunctionalTest):
         )
 
         # Ela tenta novamwente com um texto para o item, e isso agora funciona
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Buy milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # De forma perversa, ela agora decide submeter um segundo item em
         # branco na lista
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # Ela recebe um aviso semelhante na pagina da lista
         self.wait_for(lambda: self.assertEqual(
@@ -33,9 +33,9 @@ class ItemValidationTest(FunctionalTest):
             )
         )
 
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Make tea')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         # E ela pode corrigir isso preenchendo o item com um texto
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
-        
+
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -17,7 +17,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -35,7 +35,7 @@ class NewVisitorTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         # Ela insere "Use peacock feathers to make a fly"
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
@@ -54,7 +54,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith inicia uma nova lista de tarefas
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -78,7 +78,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis inicia uma nova lista inserindo um item novo. Ele é menos interessante que Edith
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
