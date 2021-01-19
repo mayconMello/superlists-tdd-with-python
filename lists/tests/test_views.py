52045741126cf129bd99a7fbe88e13ad537fdac6
Maycon Ferreira <mayconferr.18@gmail.com>
2021-01-19 09:26:19 -0300

    Refactor forms and add comments in tests views

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,10 +1,13 @@
-from unittest import skip
-
 from django.test import TestCase
-from lists.models import Item, List
 from django.utils.html import escape
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm,
+    ExistingListItemForm,
+    EMPTY_ITEM_ERROR,
+    DUPLICATE_ITEM_ERROR
+)
+from lists.models import Item, List
 
 
 # Create your tests here.
@@ -12,10 +15,16 @@ from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
+        """
+        Testa template usado na requisição GET para pagína home
+        """
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_uses_item_form(self):
+        """
+        Testa formulário renderizado na pagina home
+        """
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
 
@@ -23,18 +32,28 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_lists_template(self):
+        """
+        Verifica o template usado para listas
+        """
+
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_passes_correct_list_to_template(self):
+        """
+        Testa se lista passada é igual a retornada no template
+        """
         List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertEqual(response.context['list'], correct_list)
 
-    def test_displays_only_item_for_that_list(self):
+    def test_displays_only_items_for_that_list(self):
+        """
+        Testa mostrar somente itens para uma lista
+        """
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
         Item.objects.create(text='itemey 2', list=correct_list)
@@ -51,6 +70,9 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
+        """
+        Testa requisição POST para uma lista exsitente
+        """
         List.objects.create()
         correct_list = List.objects.create()
 
@@ -65,6 +87,9 @@ class ListViewTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
     def test_POST_redirects_to_list_view(self):
+        """
+        Testa redirecionamento após a requisição POST
+        """
         List.objects.create()
         correct_list = List.objects.create()
 
@@ -76,9 +101,13 @@ class ListViewTest(TestCase):
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
     def test_displays_item_form(self):
+        """
+        Testa vizualização do formulário
+        """
+
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def post_invalid_input(self):
@@ -89,32 +118,46 @@ class ListViewTest(TestCase):
         )
 
     def test_for_invalid_input_nothing_saved_to_db(self):
+        """
+        Testa se um post inválido não é salvo no DB
+        """
         self.post_invalid_input()
         self.assertEqual(Item.objects.count(), 0)
 
     def test_for_invalid_input_renders_list_template(self):
+        """
+        Testa o template rederizado para uma requisição inválida
+        """
         response = self.post_invalid_input()
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
 
     def test_for_invalid_input_passes_form_to_template(self):
+        """
+        Testa o form usado em uma requisição inválida
+        """
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
+        """
+        Testa a mensagem de erro para uma requisição inválida
+        """
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
-        list1 = List.objects.create()
-        item1 = Item.objects.create(list=list1, text='textey')
+        """
+        Testa requisição post de item duplicado em uma lista
+        """
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='textey')
         response = self.client.post(
-            f'/lists/{list1.id}/',
+            f'/lists/{list_.id}/',
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
