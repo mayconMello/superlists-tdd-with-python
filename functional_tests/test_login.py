2a5a81d92ba251bcd0abf7ea3c7e9300855c6d2b
Maycon Ferreira <mayconferr.18@gmail.com>
2021-01-20 17:22:38 -0300

    Added function for logout and FT and unit tests

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -49,3 +49,13 @@ class LoginTest(FunctionalTest):
 
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
+
+        # Agora ela faz logout
+        self.browser.find_element_by_link_text('Log out').click()
+
+        # Ela não está mais logada!
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(TEST_EMAIL, navbar.text)
